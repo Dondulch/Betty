44cdb0d5b7ecd6891401ff6e5755978695bae6de
Dondulch <arnoldanokwurumadu@gmai.com>
2022-09-12 00:34:48 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,33 @@
-#include <stdio.h>
+#include <stdio.h>                                                
 
-/**
- * main - Print the string in the put function
- *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
- * Return: 0
- * /
-int main(void)
-{
-     puts("\"Programming is like building a multilingual puzzle");     return (0);
-}
+                                                                  
+
+/**                                                               
+
+ * main - Print the string in the put function                    
+
+ *                                                                
+
+ * Description: using the main function                           
+
+ * this program prints "programming is like building a multilingua
+
+l puzzle"                                                         
+
+ * Return: 0                                                      
+
+ * /                                                              
+
+int main(void)                                                    
+
+{                                                                 
+
+     puts("\"Programming is like building a multilingual puzzle");
+
+     return (0);                                                  
+
+} 
+
+
+[D[A[A[B[B[A
+
